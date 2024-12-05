021f25f0602611abc9ae04c81e38baef2e646777
DocBrown101 <docbrown@tutanota.de>
2024-12-05 09:04:34 +0100

    update

--- include/keyboardHelper.h
+++ include/keyboardHelper.h
@@ -1,75 +1,99 @@
 #ifndef keyboardHelper_H /* include guards */
 #define keyboardHelper_H
 
-#include <Arduino.h>
-#include "USBKeyboard.h"
+#define HID_CUSTOM_LAYOUT
+#define LAYOUT_GERMAN
 
-// https://github.com/arduino/ArduinoCore-mbed/blob/master/libraries/USBHID/src/USBKeyboard.h
-USBKeyboard keyboard;
+#include <Arduino.h>
+#include "HID-Project.h"
+
+void initKeyboard()
+{
+  Keyboard.begin();
+}
 
 // https://code.visualstudio.com/shortcuts/keyboard-shortcuts-windows.pdf
 void vsCodeCommentCodeBlock()
 {
-  keyboard.key_code('a', KEY_ALT | KEY_SHIFT); // Alt+Shift+A - Toggle block comment
+  // Alt+Shift+A - Toggle block comment
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press('a');
+
+  delay(100);
+  Keyboard.releaseAll();
 }
 
 void vsCodeFormatSelection()
 {
-  keyboard.key_code('k', KEY_CTRL); // Ctrl+K 
+  // keyboard.key_code('k', KEY_CTRL); // Ctrl+K
   delay(100);
-  keyboard.key_code('f', KEY_CTRL); // Ctrl+F
+  // keyboard.key_code('f', KEY_CTRL); // Ctrl+F
 }
 
 // https://support.microsoft.com/en-us/office/keyboard-shortcuts-for-microsoft-teams-2e8e2a70-e8d8-4a19-949b-4c36dd5292d2
 void msTeamsToggleRaiseHand()
 {
-  keyboard.key_code('k', KEY_CTRL | KEY_SHIFT); // Ctrl+Shift+K - Raise or lower your hand
+  // Ctrl+Shift+K - Raise or lower your hand
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press('k');
+
+  delay(100);
+  Keyboard.releaseAll();
 }
 
 void msTeamsToggleVideo()
 {
-  keyboard.key_code('o', KEY_CTRL | KEY_SHIFT); // Ctrl+Shift+O - Toggle video
+  // keyboard.key_code('o', KEY_CTRL | KEY_SHIFT); // Ctrl+Shift+O - Toggle video
 }
 
 void msTeamsToggleMute()
 {
-  keyboard.key_code('m', KEY_CTRL | KEY_SHIFT); // Ctrl+Shift+M - Toggle mute
+  // keyboard.key_code('m', KEY_CTRL | KEY_SHIFT); // Ctrl+Shift+M - Toggle mute
 }
 
 void msTeamsStartScreenShare()
 {
-  keyboard.key_code('e', KEY_CTRL | KEY_SHIFT); // Ctrl+Shift+E - Start screen share
+  // keyboard.key_code('e', KEY_CTRL | KEY_SHIFT); // Ctrl+Shift+E - Start screen share
 }
 
 void msTeamsEndCall()
 {
-  keyboard.key_code('h', KEY_CTRL | KEY_SHIFT); // Ctrl+Shift+H - End audio / video call
+  // keyboard.key_code('h', KEY_CTRL | KEY_SHIFT); // Ctrl+Shift+H - End audio / video call
 }
 
-void mediaToggleMute() {
-  keyboard.media_control(KEY_MUTE);
+void mediaToggleMute()
+{
+  Keyboard.write(KEY_VOLUME_MUTE);
+  // keyboard.media_control(KEY_MUTE);
 }
 
-void mediaVolumeUp() {
-  keyboard.media_control(KEY_VOLUME_UP);
+void mediaVolumeUp()
+{
+  // keyboard.media_control(KEY_VOLUME_UP);
 }
 
-void mediaVolumeUpDelayed() {
-  keyboard.media_control(KEY_VOLUME_UP);
+void mediaVolumeUpDelayed()
+{
+  // keyboard.media_control(KEY_VOLUME_UP);
   delay(75);
 }
 
-void mediaVolumeDown() {
-  keyboard.media_control(KEY_VOLUME_DOWN);
+void mediaVolumeDown()
+{
+  // keyboard.media_control(KEY_VOLUME_DOWN);
 }
 
-void mediaVolumeDownDelayed() {
-  keyboard.media_control(KEY_VOLUME_DOWN);
+void mediaVolumeDownDelayed()
+{
+  // keyboard.media_control(KEY_VOLUME_DOWN);
   delay(75);
 }
 
-void sendDropboxUrl() {
-  keyboard.printf("https://www.dropbox.com/account/plan?role=personal");
+void sendDropboxUrl()
+{
+  Keyboard.print("https://www.dropbox.com/account/plan?role=personal");
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,63 @@
 #include <Arduino.h>
 #include <OneButton.h>
-#include <vector>
 #include "keyboardHelper.h"
-
-std::vector<OneButton> buttons;
 
 /*
 Physical layout of the USB Macro Pad with button functions:
 
 +-----------------------------------+------------------------------------+-----------------------------------+
-| btn3 (black)                      | btn2 (green)                       | btn1 (red)                        |
+| buttons[2] (black)                | buttons[1] (green)                 | buttons[0] (red)                  |
 | Pin: 18                           | Pin: 17                            | Pin: 16                           |
 | Click: mediaToggleMute            | Click: msTeamsToggleVideo          | Click: msTeamsToggleMute          |
 | LongPress: msTeamsToggleRaiseHand | LongPress: msTeamsStartScreenShare | LongPress: msTeamsEndCall         |
 +-----------------------------------+------------------------------------+-----------------------------------+
-| btn4 (left)                       | btn5 (middle)                      | btn6 (right)                      |
+| buttons[3] (left)                 | buttons[4] (middle)                | buttons[5] (right)                |
 | Pin: 10                           | Pin: 11                            | Pin: 12                           |
 | Click: mediaVolumeUp              | Click: mediaVolumeDown             | Click: vsCodeCommentCodeBlock     |
 | Hold: mediaVolumeUpDelayed        | Hold: mediaVolumeDownDelayed       | LongPress: vsCodeFormatSelection  |
 +-----------------------------------+------------------------------------+-----------------------------------+
 */
-OneButton btn1(16);
-OneButton btn2(17);
-OneButton btn3(18);
-OneButton btn4(10);
-OneButton btn5(11);
-OneButton btn6(12);
+OneButton buttons[6] = {
+    OneButton(16), // Button 0
+    OneButton(17), // Button 1
+    OneButton(18), // Button 2
+    OneButton(10), // Button 3
+    OneButton(11), // Button 4
+    OneButton(12)  // Button 5
+};
 
 void setup()
 {
-  btn1.attachClick(msTeamsToggleMute);
-  btn2.attachClick(msTeamsToggleVideo);
-  btn3.attachClick(mediaToggleMute);
-  btn4.attachClick(mediaVolumeUp);
-  btn5.attachClick(mediaVolumeDown);
-  btn6.attachClick(vsCodeCommentCodeBlock);
+  buttons[0].attachClick(msTeamsToggleMute);
+  buttons[1].attachClick(msTeamsToggleVideo);
+  buttons[2].attachClick(mediaToggleMute);
+  buttons[3].attachClick(mediaVolumeUp);
+  buttons[4].attachClick(mediaVolumeDown);
+  buttons[5].attachClick(vsCodeCommentCodeBlock);
 
-  btn1.attachLongPressStart(msTeamsEndCall);
-  btn2.attachLongPressStart(msTeamsStartScreenShare);
-  btn3.attachLongPressStart(msTeamsToggleRaiseHand);
-  btn6.attachLongPressStart(vsCodeFormatSelection);
+  buttons[0].attachLongPressStart(msTeamsEndCall);
+  buttons[1].attachLongPressStart(msTeamsStartScreenShare);
+  buttons[2].attachLongPressStart(msTeamsToggleRaiseHand);
+  buttons[5].attachLongPressStart(vsCodeFormatSelection);
 
-  btn5.attachDoubleClick(sendDropboxUrl);
+  buttons[4].attachDoubleClick(sendDropboxUrl);
 
-  btn4.attachDuringLongPress(mediaVolumeUpDelayed);
-  btn5.attachDuringLongPress(mediaVolumeDownDelayed);
+  buttons[3].attachDuringLongPress(mediaVolumeUpDelayed);
+  buttons[4].attachDuringLongPress(mediaVolumeDownDelayed);
 
-  buttons.push_back(btn1);
-  buttons.push_back(btn2);
-  buttons.push_back(btn3);
-  buttons.push_back(btn4);
-  buttons.push_back(btn5);
-  buttons.push_back(btn6);
-
-  for (uint i = 0; i < buttons.size(); i++)
+  for (OneButton &button : buttons)
   {
-    buttons.at(i).setDebounceMs(40);
-    buttons.at(i).setClickMs(150);
+    button.setDebounceMs(40);
+    button.setClickMs(150);
   }
+
+  initKeyboard();
 }
 
 void loop()
 {
-  for (uint i = 0; i < buttons.size(); i++)
+  for (OneButton &button : buttons)
   {
-    buttons.at(i).tick();
+    button.tick();
   }
 }
